1521e7a47af28ba2102a17cbc6d96fe1bc8d64a6
courtney <rebocourtney@gmail.com>
2022-03-09 15:25:19 -0800

    created options array

--- Account.java
+++ Account.java
@@ -10,5 +10,21 @@ public class Account {
   public void showMenu() {
     System.out.println("Welcome, " + this.name + "!");
     System.out.println("Your account ID is: " + this.id);
+    System.out.println("\nWhat would you like to do?\n");
+
+    String[] options = {
+        "A. Check your balance",
+        "B. Make a deposit",
+        "C. Make a withdrawal",
+        "D. View previous transaction",
+        "E. Calculate interest",
+        "F. Exit"
+    };
+
+    for (String option : options) {
+      System.out.println(option);
+    }
+
+    System.out.println("\nEnter an option: ");
   }
 }
